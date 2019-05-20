04e52e4d21b3d21ee7b35f70d4c5edcf3cf6c060
JaguarKat <35699118+JaguarKat@users.noreply.github.com>
2019-05-20 04:05:28 +0300

    die

--- encoder.cpp
+++ encoder.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
 int main()
 {
 	cout << "Enter the name of the first file:";
+	String deUnde;
+	cin >> deUnde;
+	ifstream citeste(deUnde + ".argin");
 	system("pause");
 	return 0;
 }
