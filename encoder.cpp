c25b4749310cf24fa83ebc7e736d071250246dc7
JaguarKat <stereteodorioanin@gmail.com>
2019-05-20 04:17:34 +0300

    die

--- encoder.cpp
+++ encoder.cpp
@@ -5,10 +5,14 @@ using namespace std;
 
 int main()
 {
-	cout << "Enter the name of the first file:";
-	String deUnde;
+	cout << "Enter the name of the first file: ";
+	string deUnde;
 	cin >> deUnde;
 	ifstream citeste(deUnde + ".argin");
+	cout << "Enter the name of the second file: ";
+	string unde;
+	cin >> unde;
+	ofstream scrie(unde + ".argout");
 	system("pause");
 	return 0;
 }
